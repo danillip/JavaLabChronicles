1cfe6e7fb184c1e9d11a329bff6f08bb228c5d4b
danillip <danillip2@inbox.ru>
2025-02-18 12:35:51 +0300

    Создана базовая структура класса IntContainer

---
+++ Lab1/src/main/java/org/example/IntContainer.java
@@ -0,0 +1,7 @@
+package org.example;
+
+// Начальная версия класса IntContainer
+// TODO: Реализовать хранение целых чисел
+public class IntContainer {
+    // Пока без реализации хранения элементов
+}
