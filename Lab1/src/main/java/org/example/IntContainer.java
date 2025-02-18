18476795f2cc01ffddba0e9f95972ea6c7e6fff5
danillip <danillip2@inbox.ru>
2025-02-18 12:41:39 +0300

    Начальная версия класса IntContainer с полями и базовым методом add

--- Lab1/src/main/java/org/example/IntContainer.java
+++ Lab1/src/main/java/org/example/IntContainer.java
@@ -1,7 +1,14 @@
 package org.example;
 
-// Начальная версия класса IntContainer
-// TODO: Реализовать хранение целых чисел
+// Начальная версия класса IntContainer с полями и базовым методом add
+// TODO: добавить методы get и remove, а также проверку индекса
 public class IntContainer {
-    // Пока без реализации хранения элементов
+    // TODO: возможно, увеличить размер начального массива
+    private int[] data = new int[10];
+    private int size = 0;
+
+    // Метод для добавления элемента (без проверки вместимости)
+    public void add(int value) {
+        data[size++] = value;
+    }
 }
