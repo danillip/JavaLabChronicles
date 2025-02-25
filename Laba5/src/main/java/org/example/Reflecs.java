0b5cd3c14986013874374faa6de30a383c12aa96
danillip <danillip2@inbox.ru>
2025-02-25 22:26:25 +0300

    Laba5 - наконец настройка Reflecs

--- Laba5/src/main/java/org/example/Reflecs.java
+++ Laba5/src/main/java/org/example/Reflecs.java
@@ -1,17 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.beans.SomeBean;
+import org.example.injector.Injector;
+
+/**
+ * Reflecs main(), чтобы запустить и проверить
+ * TODO: Запустить, убедиться, что выводит "A" + "C"
+ */
 public class Reflecs {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SomeBean sb = new SomeBean();
+        new Injector().inject(sb);
+        sb.foo(); // ожидаем "A" и "C"
     }
 }
