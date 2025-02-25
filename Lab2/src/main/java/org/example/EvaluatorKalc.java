b93b66c116ba191f70592028375238eda61aeafc
danillip <danillip2@inbox.ru>
2025-02-25 16:32:59 +0300

    Laba2 - Старт проекта

---
+++ Lab2/src/main/java/org/example/EvaluatorKalc.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * TODO: Первая версия программы - просто проверим, что проект собирается
+ * UPD: Добавить логику вычисления выражений
+ */
+public class EvaluatorKalc {
+    public static void main(String[] args) {
+        System.out.println("Hello from EvaluatorKalc!");
+        // TODO: Реализовать парсер и вычислитель выражений
+    }
+}
---
+++ Lab2/src/test/java/org/example/EvaluatorKalcTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TODO: Написать тесты позже.
+ */
+public class EvaluatorKalcTest {
+
+    @Test
+    public void testPlaceholder() {
+        // TODO: Добавить реальные тесты
+        assertTrue(true);
+    }
+
+}
