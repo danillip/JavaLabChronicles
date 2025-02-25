36f4a3dc3ac91e6e785c0bd4e8605d5456920a1e
danillip <danillip2@inbox.ru>
2025-02-25 20:24:03 +0300

    Lab3 - Тестировани remove

--- Lab3/src/main/java/com/example/CollectionWork.java
+++ Lab3/src/main/java/com/example/CollectionWork.java
@@ -6,20 +6,22 @@ import java.util.List;
 
 /**
  * Основной класс для сравнения производительности ArrayList и LinkedList
+ *
+ *  - Добавлен метод testRemovePerformance
+ *  - Добавлена вызовы методов в main
  */
 public class CollectionWork {
-    private static final int ITERATIONS = 1488;
+    private static final int ITERATIONS = 1000;
 
     public static void main(String[] args) {
-        System.out.println("Тест производительности: add");
+        System.out.println("Тест производительности: add, remove");
 
-        // Тесты
         testAddPerformance(ITERATIONS);
+        testRemovePerformance(ITERATIONS);
     }
 
     /**
-     * Тестируем скорость добавления (add) для ArrayList и LinkedList
-     * @param iterations количество элементов, которые добавляем
+     * Тест добавления
      */
     private static void testAddPerformance(int iterations) {
         // ArrayList
@@ -40,13 +42,45 @@ public class CollectionWork {
         long endLinkedAdd = System.nanoTime();
         long durationLinkedAdd = endLinkedAdd - startLinkedAdd;
 
-        // Резы
         System.out.println("----- Add Performance -----");
         System.out.println("Iterations: " + iterations);
         System.out.println("ArrayList add time (ns):   " + durationArrayAdd);
         System.out.println("LinkedList add time (ns): " + durationLinkedAdd);
+    }
 
-        // TODO: Добавить запись результатов в какую-то общую структуру или таблицу
-        // UPD: Просто печать
+    /**
+     * Тестируем (remove)
+     * Удаляем из конца списка для упрощения
+     */
+    private static void testRemovePerformance(int iterations) {
+        // ArrayList
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < iterations; i++) {
+            arrayList.add(i);
+        }
+        long startArrayRemove = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            // Ремув с конца
+            arrayList.remove(arrayList.size() - 1);
+        }
+        long endArrayRemove = System.nanoTime();
+        long durationArrayRemove = endArrayRemove - startArrayRemove;
+
+        // LinkedList
+        List<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < iterations; i++) {
+            linkedList.add(i);
+        }
+        long startLinkedRemove = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            linkedList.remove(linkedList.size() - 1);
+        }
+        long endLinkedRemove = System.nanoTime();
+        long durationLinkedRemove = endLinkedRemove - startLinkedRemove;
+
+        System.out.println("----- Remove Performance -----");
+        System.out.println("Iterations: " + iterations);
+        System.out.println("ArrayList remove time (ns):   " + durationArrayRemove);
+        System.out.println("LinkedList remove time (ns): " + durationLinkedRemove);
     }
 }
