e4e0cc566104616b96c59e5a1afb54035e128be3
danillip <danillip2@inbox.ru>
2025-02-25 17:32:53 +0300

    Laba2 - Generate Doc

---
+++ Lab2/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"EvaluatorKalc"},{"p":"org.example","l":"EvaluatorKalcTest"}];updateSearchResults();
