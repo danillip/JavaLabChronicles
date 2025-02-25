343d804a6d4a023132c1c3f2e790065fee793b4a
danillip <danillip2@inbox.ru>
2025-02-25 21:12:19 +0300

    Laba4 - Добавляем заготовку классов Person и Division

---
+++ Lab4/src/main/java/org/example/Division.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Класс Division хранит данные о подразделении
+ */
+public class Division {
+    private static long divisionIdCounter = 1;
+
+    private long id;
+    private String name;
+
+    // TODO: реализовать конструкторы, геттеры/сеттеры, переопределить toString
+    // TODO: при создании нового Division автоматически генерировать ID
+}
---
+++ Lab4/src/main/java/org/example/Person.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.time.LocalDate;
+
+/**
+ * Класс Person хранит данные о человеке
+ */
+public class Person {
+    private long id;
+    private String name;
+    private String gender;
+    private LocalDate birthDate;
+    private Division division;
+    private double salary;
+
+    // TODO: реализовать конструкторы, геттеры/сеттеры, переопределить toString
+}
