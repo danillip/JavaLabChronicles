48a6ab504e079499b6d946c09e833aba6f6be210
danillip <danillip2@inbox.ru>
2025-02-25 21:13:54 +0300

    Laba4 - UPD Person и Division - добавляем конструкторы, геттеры/сеттеры, toString

--- Lab4/src/main/java/org/example/Division.java
+++ Lab4/src/main/java/org/example/Division.java
@@ -1,14 +1,36 @@
 package org.example;
 
-/**
- * Класс Division хранит данные о подразделении
- */
 public class Division {
     private static long divisionIdCounter = 1;
 
     private long id;
     private String name;
 
-    // TODO: реализовать конструкторы, геттеры/сеттеры, переопределить toString
-    // TODO: при создании нового Division автоматически генерировать ID
+    public Division() {
+        // При создании без имени – просто примерчик
+        this.id = divisionIdCounter++;
+        this.name = "Unknown Division";
+    }
+
+    public Division(String name) {
+        this.id = divisionIdCounter++;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Division{id=" + id + ", name='" + name + "'}";
+    }
 }
--- Lab4/src/main/java/org/example/Person.java
+++ Lab4/src/main/java/org/example/Person.java
@@ -2,9 +2,6 @@ package org.example;
 
 import java.time.LocalDate;
 
-/**
- * Класс Person хранит данные о человеке
- */
 public class Person {
     private long id;
     private String name;
@@ -13,5 +10,70 @@ public class Person {
     private Division division;
     private double salary;
 
-    // TODO: реализовать конструкторы, геттеры/сеттеры, переопределить toString
+    public Person() {
+        // Конструктор по умолчанию
+    }
+
+    public Person(long id, String name, String gender, LocalDate birthDate, Division division, double salary) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.division = division;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Division getDivision() {
+        return division;
+    }
+
+    public void setDivision(Division division) {
+        this.division = division;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person{id=%d, name='%s', gender='%s', birthDate=%s, division=%s, salary=%.2f}",
+                id, name, gender, birthDate, division != null ? division.getName() : "null", salary);
+    }
 }
