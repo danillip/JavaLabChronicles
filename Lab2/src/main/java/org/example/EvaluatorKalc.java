399f5b4e98975417b810ccbefcf2d6039da53d10
danillip <danillip2@inbox.ru>
2025-02-25 16:35:29 +0300

    Laba2 - Добавили базовую логику парсинга + TODO

--- Lab2/src/main/java/org/example/EvaluatorKalc.java
+++ Lab2/src/main/java/org/example/EvaluatorKalc.java
@@ -1,26 +1,32 @@
 package org.example;
 
 /**
- * Основной класс для вычисления выражений
- * TODO: Парсер пока что возращает null - нужно доработать
- * UPD: Дальше будем добавлять аналитику строк и т.д
+ * UPD: Добавили простейший парсинг: разделение строки по знакам + и -
  */
 public class EvaluatorKalc {
 
     /**
-     * Временная заглушка для парсинга
+     * Разбирает выражение, пока только учитываем знаки + и -
+     * TODO: добавить обработку умножения, деления, скобок
      * @param expr математическое выражение
-     * @return пока null
+     * @return черновой результат (как строка для наглядности)
      */
-    public static Object parseExpression(String expr) {
-        // TODO: Реализовать разбор выражения
-        return null;
+    public static String parseExpression(String expr) {
+        // Черновая реализация
+        // Допустим, разобьем строку по + и - и выведем частички
+        String[] tokens = expr.split("(?=[+-])|(?<=[+-])");
+        StringBuilder sb = new StringBuilder("Tokens: ");
+        for (String t : tokens) {
+            sb.append("[").append(t).append("] ");
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello from EvaluatorKalc!");
-        // Пробуем вызвать парсер
-        Object result = parseExpression("1+2");
-        System.out.println("Парсер вернул: " + result);
+        System.out.println("=== EvaluatorKalc v0.1 ===");
+        String testExpr = "1+2-3+10";
+        String result = parseExpression(testExpr);
+        System.out.println("Исходное выражение: " + testExpr);
+        System.out.println("Результат парсинга: " + result);
     }
 }
