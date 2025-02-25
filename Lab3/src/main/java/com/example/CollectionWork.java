0fa3ff74d22d997e79eee7527dd627cb3408fed9
danillip <danillip2@inbox.ru>
2025-02-25 20:12:02 +0300

    Lab3 - Создаём заготовку проекта

---
+++ Lab3/src/main/java/com/example/CollectionWork.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * TODO: Начало работы над классом для сравнения производительности ArrayList и LinkedList
+ * UPD: Пока только пустая заготовка
+ */
+public class CollectionWork {
+    public static void main(String[] args) {
+        // TODO: Здесь будет запуск логики для тестирования производительности
+        System.out.println("Hello from CollectionWork!");
+    }
+}
---
+++ Lab3/src/test/java/com/example/CollectionWorkTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TODO: Заготовка для будущих юнит-тестов
+ * UPD: Пока оставим всё пустым
+ */
+public class CollectionWorkTest {
+
+    @Test
+    void sampleTest() {
+        // TODO: В будущих коммитах напишем реальные тесты
+        assertTrue(true, "Пока что простой тест");
+    }
+}
