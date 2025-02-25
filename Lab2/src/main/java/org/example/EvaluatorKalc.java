710ae3e56c605aa3e1d7809b5be047050b5a3546
danillip <danillip2@inbox.ru>
2025-02-25 16:33:53 +0300

    Laba2 - Начало работы над парсером

--- Lab2/src/main/java/org/example/EvaluatorKalc.java
+++ Lab2/src/main/java/org/example/EvaluatorKalc.java
@@ -1,12 +1,26 @@
 package org.example;
 
 /**
- * TODO: Первая версия программы - просто проверим, что проект собирается
- * UPD: Добавить логику вычисления выражений
+ * Основной класс для вычисления выражений
+ * TODO: Парсер пока что возращает null - нужно доработать
+ * UPD: Дальше будем добавлять аналитику строк и т.д
  */
 public class EvaluatorKalc {
+
+    /**
+     * Временная заглушка для парсинга
+     * @param expr математическое выражение
+     * @return пока null
+     */
+    public static Object parseExpression(String expr) {
+        // TODO: Реализовать разбор выражения
+        return null;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello from EvaluatorKalc!");
-        // TODO: Реализовать парсер и вычислитель выражений
+        // Пробуем вызвать парсер
+        Object result = parseExpression("1+2");
+        System.out.println("Парсер вернул: " + result);
     }
 }
--- Lab2/src/test/java/org/example/EvaluatorKalcTest.java
+++ Lab2/src/test/java/org/example/EvaluatorKalcTest.java
@@ -1,18 +1,13 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * TODO: Написать тесты позже.
- */
 public class EvaluatorKalcTest {
 
     @Test
     public void testPlaceholder() {
-        // TODO: Добавить реальные тесты
+        // Пока оставляем
         assertTrue(true);
     }
-
 }
